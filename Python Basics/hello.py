77c102b4c104ccebd71971196c06a5228d4a516e
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-05-29 18:10:07 +0200

    Using If condition
    Using if condition to check the users name and send a special message to each user.

--- Python Basics/hello.py
+++ Python Basics/hello.py
@@ -2,4 +2,8 @@ first_name = input("What's your first name? ")
 
 print(first_name)
 print("Hello, ", first_name)
-print(first_name, " is here to study python")
+if first_name == "Jim":
+    print(first_name, " is here to study python")
+else:
+    print("You should totaly learn python {}!".format(first_name))
+print("Have a great day {}!".format(first_name))
