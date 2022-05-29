0e5d357fc48d949ff93e9ea7a9943e32d33f7119
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-05-29 18:43:50 +0200

    checking users age
    in case you have a user who cannot read yet.

--- Python Basics/hello.py
+++ Python Basics/hello.py
@@ -5,7 +5,11 @@ print("Hello, ", first_name)
 if first_name == "Jim":
     print(first_name, " is here to study python")
 elif first_name == "Amr":
-    print(first_name, "is learning with fellow students in the community! Me too!!")
+    print(first_name, "is learning with fellow students in the community! Me!")
 else:
+    # This is just in case you have a younger user who can't read yet
+    age = int(input("How old are you?  "))
+    if age <= 6:
+        print("Wow you are {}! if you are confident with your reading already...".format(age))
     print("You should totaly learn python {}!".format(first_name))
 print("Have a great day {}!".format(first_name))
