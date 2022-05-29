18d7da6a0c5ac4436179dbce7ac3f9c713a87df1
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-05-29 18:32:46 +0200

    Using Elif
    Using Elif condition

--- Python Basics/hello.py
+++ Python Basics/hello.py
@@ -4,6 +4,8 @@ print(first_name)
 print("Hello, ", first_name)
 if first_name == "Jim":
     print(first_name, " is here to study python")
+elif first_name == "Amr":
+    print(first_name, "is learning with fellow students in the community! Me too!!")
 else:
     print("You should totaly learn python {}!".format(first_name))
 print("Have a great day {}!".format(first_name))
