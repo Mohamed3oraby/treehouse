130f06362a671319d084bb06a079b106db24064b
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-06-01 14:52:50 +0200

    taking dynamic input from user
    adding input to take dynamic input from users

--- check_please.py
+++ check_please.py
@@ -5,5 +5,11 @@ def split_check (total, number_of_people):
     cost_per_person = math.ceil(total / number_of_people)
     return cost_per_person
 
-amount_due = split_check (84.79, 4)
+
+total_due = float(input("What is the total?  "))  # using float to change from string to float
+number_of_people = int(input("How many people?  "))
+
+amount_due = split_check (total_due, number_of_people)
+
+
 print ("Each person owes ${}".format(amount_due))
