a7e8117a2bd624d0458de3a962d9ce340404fcb1
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-06-01 14:48:42 +0200

    round up the cost
    using math module to call ceil function to round up the cost per person

--- check_please.py
+++ check_please.py
@@ -1,4 +1,9 @@
+import math
+
+
 def split_check (total, number_of_people):
-    cost_per_person = total / number_of_people
+    cost_per_person = math.ceil(total / number_of_people)
     return cost_per_person
 
+amount_due = split_check (84.79, 4)
+print ("Each person owes ${}".format(amount_due))
