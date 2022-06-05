c216f089a052059213efa5208f45c80968b43ebd
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-06-05 16:13:19 +0200

    adding an exception that handles zeroDivision error
    using except ValueError *as* err:
    to print another friendly error to inform user to use a valid number.

--- check_please.py
+++ check_please.py
@@ -2,14 +2,18 @@ import math
 
 
 def split_check (total, number_of_people):
+    if number_of_people <=1:
+        raise ValueError("More than 1 person is required to split the check.")
     return math.ceil(total / number_of_people)
 
 #Checking for errors
 try:
     total_due = float(input("What is the total?  "))  # using float to change from string to float
     number_of_people = int(input("How many people?  "))
-except ValueError:                                      
-    print("Oh No! that's not a valid value, Try again...")    
-else:
     amount_due = split_check (total_due, number_of_people)
+
+except ValueError as err:                                      
+    print("Oh No! that's not a valid value, Try again...")  
+    print("({})".format(err))  
+else:
     print ("Each person owes ${}".format(amount_due))
