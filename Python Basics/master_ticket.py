c199939da7596a79d6bc31717b088443e8831629
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-06-05 18:34:05 +0200

    adding service charge

--- Python Basics/master_ticket.py
+++ Python Basics/master_ticket.py
@@ -1,23 +1,25 @@
+SERVICE_CHARGE = 2
 TICKET_PRICE = 10
 
 tickets_remaining = 100
 
+# Create calculate_price function. It take number of tickets and returns 
+# number_of_ticktes * TICKET_PRICE
+def calculate_price(number_of_tickets):
+    return (number_of_tickets * TICKET_PRICE) + SERVICE_CHARGE
 
 while tickets_remaining >= 1:
     print("There're {} tickets remaining".format(tickets_remaining))
     user_name = input("what is your name? ")
     number_of_tickets = input("Hello {}, How many tickets would you like to buy? ".format(user_name))
-    # Excpect a ValueError to happen and handle it appropriately .. Remember to test it out!
     try:
         number_of_tickets = int(number_of_tickets) 
-        # Raise a ValueError if the request is more than the available tickets
         if number_of_tickets > tickets_remaining:
             raise ValueError("There're onlye {} tickets remaining.".format(tickets_remaining))
     except ValueError as err: 
-        # Include the error text output 
         print("Oh No, we ran into an issue. {}. Please try again. ".format(err))
     else:
-        total_price = (number_of_tickets * TICKET_PRICE)
+        total_price = calculate_price(number_of_tickets)
         print("Amount due is ${}".format(total_price))
         should_proceed = input("Do you want to proceed?  Y/N  ")
         if should_proceed.lower() == "y":
