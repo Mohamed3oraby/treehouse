4d3948d8bdb6f18d23a7c397b56282cd9156b802
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-06-05 17:41:45 +0200

    adding inputs to gather username and how many tickets they would like to buy
    asking the user for his name
    welcoming the user by his name
    asking the user how many tickets he would like to buy
    informing the user the price for the requested tickets.

--- Python Basics/master_ticket.py
+++ Python Basics/master_ticket.py
@@ -7,5 +7,12 @@ tickets_remaining = 100
 print("There're {} tickets remaining".format(tickets_remaining))
 
 # Gather the user's name and assign it to avriable 
-
 user_name = input("what is your name? ")
+
+# Prompt the user by name and ask him how many tickets they would like
+number_of_tickets = input("Hello {}, How many tickets would you like to buy? ".format(user_name))
+number_of_tickets = int(number_of_tickets) # Convert input to integer
+# Calculate the price (number of tickets muliplied by the price) and assign that to a variable
+total_price = (number_of_tickets * TICKET_PRICE)
+# Output the price to the screen
+print("Amount due is ${}".format(total_price))
