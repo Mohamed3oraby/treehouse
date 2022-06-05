2f978de0e13f12af027377333c4018113a512fab
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-06-05 17:26:15 +0200

    Output How many tickets are remaining

---
+++ Python Basics/master_ticket.py
@@ -0,0 +1,11 @@
+TICKET_PRICE = 10
+
+tickets_remaining = 100
+
+# Output How many tickets are remaining using the tickets_remaining variable 
+
+print("There're {} tickets remaining".format(tickets_remaining))
+
+# Gather the user's name and assign it to avriable 
+
+user_name = input("what is your name? ")
