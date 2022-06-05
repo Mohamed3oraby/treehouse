d288dcdd76980f90b91b9c7da10a7f8f06ed500c
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-06-05 13:29:24 +0200

    adding exceptions for errors
    printing a friendly messages for the users when they face an error

--- check_please.py
+++ check_please.py
@@ -4,11 +4,12 @@ import math
 def split_check (total, number_of_people):
     return math.ceil(total / number_of_people)
 
-
-total_due = float(input("What is the total?  "))  # using float to change from string to float
-number_of_people = int(input("How many people?  "))
-
-amount_due = split_check (total_due, number_of_people)
-
-
-print ("Each person owes ${}".format(amount_due))
+#Checking for errors
+try:
+    total_due = float(input("What is the total?  "))  # using float to change from string to float
+    number_of_people = int(input("How many people?  "))
+except ValueError:                                      
+    print("Oh No! that's not a valid value, Try again...")    
+else:
+    amount_due = split_check (total_due, number_of_people)
+    print ("Each person owes ${}".format(amount_due))
