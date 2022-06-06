2189e11d916fc18fddfadc3c7e6899a114976b6c
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-06-06 18:05:00 +0200

    Adding More to the list
    using append and extend

---
+++ lists/meeting.py
@@ -0,0 +1,6 @@
+attendees = ["Ken","Ayah","Oraby","Hammad"]
+attendees.append("Karim")
+attendees.extend(["Amr","Basmallah"])
+optional_invitees = ["Gamal","Hoda"]
+potentional_attendees = attendees + optional_invitees
+print("There are ", len(potentional_attendees), "potentional attendees currenty")
