9894deee7aa720057f8c3d286b3bde2a5149ea80
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-06-06 14:55:50 +0200

    solving the challenge

--- Python Basics/Practice Python/looping.py
+++ Python Basics/Practice Python/looping.py
@@ -1,9 +1,17 @@
 name = input("What's your name? ")
 
 # TODO: Ask the user by name if they understand Python while loops
-
+answer = input("{}, do you understand python while loops? \nEnter (yes or no):  ".format(name))
 
 # TODO: Write a while statement that checks if the user doesn't understand while loops
+while answer.lower() != "yes" :
+    print("With the while loop we can execute a set of statements as long as a condition is true.")
+    answer = input("{}, do you understand python while loops? \nEnter (yes or no):  ".format(name))
+
+else:
+    print("Nice Job {}".format(name))
+
+
 # TODO: Since the user doesn't understand while loops, let's explain them.
 # TODO: Ask the user again, by name, if they understand while loops.
 
