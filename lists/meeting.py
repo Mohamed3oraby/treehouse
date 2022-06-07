21055e40d4fb1cab94b0e442f8c4a80330b7bf78
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-06-08 01:10:54 +0200

    update meeting

--- lists/meeting.py
+++ lists/meeting.py
@@ -4,3 +4,8 @@ attendees.extend(["Amr","Basmallah"])
 optional_invitees = ["Gamal","Hoda"]
 potentional_attendees = attendees + optional_invitees
 print("There are ", len(potentional_attendees), "potentional attendees currenty")
+
+to_line = ", ".join(attendees)
+cc_line = ", ".join(optional_invitees)
+print("To: " + to_line)
+print("CC: " + cc_line)
