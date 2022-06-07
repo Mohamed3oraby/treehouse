1d081920354200cf1fb40f25e2ee7d2392b6a050
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-06-08 01:46:15 +0200

    Adding ask for help
    Adding ask for help for user to be able to call it anytime in the app.

--- lists/shopping_list.py
+++ lists/shopping_list.py
@@ -1,5 +1,24 @@
+# Create a new empty list named shopping_list
+
+
+# Create function named add_to_list that declares a parameter named item
+    # Add item to the list. 
+
+def show_help():
+    print("What should we pick up at the store?")
+    print("""
+Enter 'DONE' to stop adding items.
+Enter 'HELP' for this help.
+""")
+
+show_help()
 while True:
     new_item = input("> ")
 
     if new_item == 'DONE':
         break
+    elif new_item == 'HELP':
+        show_help()
+        continue
+
+        # Call that list with new_item as an argument 
