cd8e334007d2bef1ff95f885088637839d867abb
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2022-06-08 02:09:29 +0200

    Updating shopping list
    Creating new function to show the items added to the list
    Updating the HELP documentation
    Calling the function at the end.

--- lists/shopping_list.py
+++ lists/shopping_list.py
@@ -1,6 +1,16 @@
 # Create a new empty list named shopping_list
 shopping_list = []
 
+
+def show_help():
+    print("What should we pick up at the store?")
+    print("""
+Enter 'DONE' to stop adding items.
+Enter 'HELP' for this help.
+Enter 'SHOW' to see your current list.
+""")
+
+
 # Create function named add_to_list that declares a parameter named item
 def add_to_list(item):
     # Add item to the list. 
@@ -9,13 +19,14 @@ def add_to_list(item):
     print("Added!, list has {} items.".format(len(shopping_list)))
 
 
+# Define function named show_list that prints all the items in the list
+def show_list():
+    print("Here's your list: ")
+    for item in shopping_list:
+        print(item)
+    
+    
 
-def show_help():
-    print("What should we pick up at the store?")
-    print("""
-Enter 'DONE' to stop adding items.
-Enter 'HELP' for this help.
-""")
 
 show_help()
 while True:
@@ -26,6 +37,11 @@ while True:
     elif new_item == 'HELP':
         show_help()
         continue
+    elif new_item == 'SHOW':
+        show_list()
+        continue
 
     # Call that list with new_item as an argument 
     add_to_list(new_item)
+ 
+show_list()
